93ec70d0b95f62688c44ae807f3b7403d4ed3fde
David López <dalo_vidpez@hotmail.com.ar>
2021-06-09 20:48:51 -0300

    Logré refinar el código. Quise poner el sonido de comer y se rompió todo

--- src/Objects/Coin-chori.gd
+++ src/Objects/Coin-chori.gd
@@ -3,6 +3,7 @@ extends Moneda
 var nombre = "chori"
 
 func _ready():
+	print(get_parent())
 	get_parent().incrementar_total(nombre)
 
 func _on_body_entered(body):
--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -2,6 +2,10 @@ extends Area2D
 class_name Moneda
 
 onready var animation_player:AnimationPlayer = get_node("AnimationPlayer")
+onready var audio_player:AudioStreamPlayer2D = get_node("Sonido_comer")
 
 func _on_body_entered(body):
+	print(audio_player)
+	print(animation_player)
+	audio_player.play(0.0)
 	animation_player.play("fade_out")
